0cc19d78ee397636ac700ac94f911b5def5418b3
Agung Nurhamidan <>
2019-09-22 16:49:50 +0700

    First commit.

---
+++ src/pbo1/nim10118017/Main.java
@@ -0,0 +1,31 @@
+package pbo1.nim10118017;
+
+/**
+ *
+ * @author Agung Nurhamidan
+ * NAMA                 : Agung Nurhamidan
+ * KELAS                : PBO1
+ * NIM                  : 10118017
+ * Deskripsi Program    : Menampilkan biodata di layar.
+ * 
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("\tBIODATA ROCKSTAR");
+        System.out.println("--------------------------------------");
+        System.out.println("NAMA\t\t: Agung Nurhamidan");
+        System.out.println("NIM\t\t: 10118017");
+        System.out.println("TEMPAT LAHIR\t: Ciamis");
+        System.out.println("TANGGAL LAHIR\t: 08 November 1998");
+        System.out.println("ALAMAT\t\t: Sekeloa, Coblong");
+        System.out.println("--------------------------------------");
+        System.out.println("HOBI\t\t: Renang, Membaca");
+        System.out.println("MAKES\t\t: Sate");
+        System.out.println("MIKES\t\t: Cendol");
+    }
+    
+}
